4775e6234d86ef80b849eda007fa815231770163
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2025-07-17 02:02:26 +0530

    Time: 2629 ms (5.03%) | Memory: 53.8 MB (5.03%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int bestClosingTime(String c) {
+        int[] op = new int[c.length()+1];
+        int[] cl = new int[c.length()+1];
+
+        int openSum =0;
+        for(int i=0; i<c.length() ; i++){
+            char o = c.charAt(i);
+            openSum = o == 'N'?openSum+1:openSum;
+            op[i+1]= openSum; 
+        }
+        int closedSum =0;
+        for(int i= c.length()-1; i>=0 ; i--){
+            char o = c.charAt(i);
+            closedSum = o == 'Y'?closedSum+1:closedSum; 
+            cl[i]= closedSum;
+        }
+
+        int max=Integer.MAX_VALUE;
+        int ind = 0;
+        print(op);
+        print(cl);
+        for(int i=0; i<cl.length; i++){
+            int ss = op[i] + cl[i];
+
+            if(ss<max){
+                max=ss;
+                ind=i;
+            }
+        }
+
+        return ind;
+
+    }
+
+    public void print(int[] ss){
+        for(int s: ss){
+            System.out.print(s);
+        }
+        System.out.println();
+
+    }
+}
