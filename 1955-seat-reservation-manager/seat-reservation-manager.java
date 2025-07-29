848fd36f773bdfd9e51db37e010631e0f8069b9b
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2025-07-30 00:53:31 +0530

    Time: 95 ms (17.48%) | Memory: 88.9 MB (58.92%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.java
+++ 1955-seat-reservation-manager/seat-reservation-manager.java
@@ -1,30 +1,19 @@
 class SeatManager {
-    // Queue<Integer> qu = new PriorityQueue<>();
-    boolean[] seat;
-    int s=0, c=0;
+
+    Queue<Integer> qu = new PriorityQueue<>();
+    // int s=0;
     public SeatManager(int n) {
-        s=n;
-        seat = new boolean[n];
+        for(int i=1; i<=n;i++){
+            qu.add(i);
+        }
     }
     
     public int reserve() {
-        int re = c;
-        // System.out.println(c);
-        seat[c]=true;
-        while(c<s){
-            if(seat[c]==false)break;
-            c++;
-        }
-        // System.out.println("new c :"+c);
-        return re+1;
+        return qu.poll();
     }
     
     public void unreserve(int seatNumber) {
-        // System.out.println("re:"+seatNumber);
-        seatNumber=seatNumber-1;
-        if(seatNumber<c)c=seatNumber;
-        // System.out.println("re new c:"+c);
-        seat[seatNumber]=false;
+        qu.add(seatNumber);
     }
 }
 
