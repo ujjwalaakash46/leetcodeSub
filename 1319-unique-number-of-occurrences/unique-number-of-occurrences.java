51b963fe50fa5f54b8b7c0243db77c817dca35bf
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2025-10-08 00:05:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (42.21%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,18 +1,31 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        for( int a : arr){
-            mp.put(a, mp.getOrDefault(a, 0)+1);
-        }
-        
-        Set<Integer> set = new HashSet<>();
-        for(Map.Entry<Integer, Integer> es: mp.entrySet()){
-            if(set.contains(es.getValue())){
-                return false;
-            }
-            set.add(es.getValue());
+        // Step 1: Find the min and max value in the array
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num < min) min = num;
+            if (num > max) max = num;
         }
 
+        // Step 2: Count the occurrences for each value using a frequency array
+        int range = max - min + 1;
+        int[] freq = new int[range];
+        for (int num : arr) {
+            freq[num - min]++;
+        }
+
+        // Step 3: Use a boolean array to track which occurrence counts have been seen
+        boolean[] seen = new boolean[arr.length + 1]; // +1 because occurrence can be up to arr.length
+        for (int count : freq) {
+            if (count > 0) {
+                if (seen[count]) {
+                    // If this occurrence count already exists, return false
+                    return false;
+                }
+                seen[count] = true;
+            }
+        }
         return true;
     }
 }
