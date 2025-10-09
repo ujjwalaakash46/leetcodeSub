c8eb38d5c16eef3565629ec809276edb013a01fe
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2025-10-10 00:08:38 +0530

    Time: 5 ms (5.29%) | Memory: 42.4 MB (9.56%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.java
+++ 1983-maximum-population-year/maximum-population-year.java
@@ -1,20 +1,34 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        TreeMap<Integer, Integer> mp = new TreeMap<>();
+        // TreeMap<Integer, Integer> mp = new TreeMap<>();
+        int[] yearf = new int[101];
 
         for(int[] p : logs){
-            mp.put(p[0], mp.getOrDefault(p[0], 0)+1);
-            mp.put(p[1], mp.getOrDefault(p[1], 0)-1);
+            // mp.put(p[0], mp.getOrDefault(p[0], 0)+1);
+            // mp.put(p[1], mp.getOrDefault(p[1], 0)-1);
+
+            yearf[p[0]-1950] += 1;
+            yearf[p[1]-1950] -= 1;
         }
 
-        int mx = Integer.MIN_VALUE;
-        int mx_c = Integer.MIN_VALUE;
+        int mx = 0;
+        int mx_c = 0;
         int mxyear = 0;
-        for(Map.Entry<Integer, Integer> en: mp.entrySet()){
-            mx_c+=en.getValue();
+        // for(Map.Entry<Integer, Integer> en: mp.entrySet()){
+        //     mx_c+=en.getValue();
+        //     if(mx_c>mx){
+        //         mx=mx_c;
+        //         mxyear=en.getKey();
+        //     }
+        // }
+
+        for(int i =0;i<yearf.length; i++){
+            
+            mx_c +=yearf[i];
             if(mx_c>mx){
                 mx=mx_c;
-                mxyear=en.getKey();
+                mxyear = 1950+i;
+                System.out.println(mxyear);
             }
         }
 
