d69b7f71c80598740e9a620481d518c63fecdef4
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2025-10-19 12:47:12 +0530

    Time: 8 ms (74.05%) | Memory: 44.9 MB (49.83%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,28 +1,44 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] freq = new int[26];
-        int l=0, r=0, n=s.length();
-        int max=0;
+        // int[] freq = new int[26];
+        // int l=0, r=0, n=s.length();
+        // int max=0;
 
-        while(r<n && l<=r){
-            // System.out.println(r);
+        // while(r<n && l<=r){
+        //     char cr = s.charAt(r);
+        //     freq[cr-'A']++; 
+        //     int maxfreq = maxfreqCal(freq);
+        //     if(r-l+1 - maxfreq <= k){
+        //         max= Math.max(max, r-l+1);
+        //     }else{
+        //         for(int i=l; i<r; i++){
+        //             freq[s.charAt(i)-'A']--;
+        //             l++;
+        //             maxfreq = maxfreqCal(freq);
+        //             if(r-l+1 - maxfreq <= k){
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     r++;
+        // }
+        // return max;
+
+        int[] freq = new int[26];
+        int l = 0, r = 0, n = s.length();
+        int max = 0;
+        int maxFreq = 0;
+
+        while (r < n) {
             char cr = s.charAt(r);
-            freq[cr-'A']++; 
-            int maxfreq = maxfreqCal(freq);
-            // System.out.println(maxfreq);
-            if(r-l+1 - maxfreq <= k){
-                // System.out.println(2);
-                max= Math.max(max, r-l+1);
-            }else{
-                // System.out.println(3);
-                for(int i=l; i<r; i++){
-                    freq[s.charAt(i)-'A']--;
-                    l++;
-                    maxfreq = maxfreqCal(freq);
-                    if(r-l+1 - maxfreq <= k){
-                        break;
-                    }
-                }
+            freq[cr - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[cr - 'A']);
+
+            if (r - l + 1 - maxFreq > k) {
+                freq[s.charAt(l) - 'A']--;
+                l++;
+            } else {
+                max = Math.max(max, r - l + 1);
             }
             r++;
         }
