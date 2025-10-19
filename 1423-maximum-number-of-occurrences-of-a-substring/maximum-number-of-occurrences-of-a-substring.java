9a1a193f5b740de21c58b19d0ee0fd9d04e8031c
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2025-10-20 01:41:40 +0530

    Time: 34 ms (84.11%) | Memory: 45.7 MB (99.60%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
@@ -1,30 +1,37 @@
 class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
-        HashMap<String, Integer> mp = new HashMap<>();
-        HashSet<Character> st = new HashSet<>();
-        int l = -1, r = minSize-1;
-        while(r<s.length()){
-            String cur = s.substring(++l,++r);
-            int stSize = reSet(cur).size();
-            if(stSize<=maxLetters){
-                mp.put(cur, mp.getOrDefault(cur,0)+1);
-            }
+        int n = s.length();
+        int[] freq = new int[26];
+        int distinct = 0;
+        HashMap<String, Integer> countMap = new HashMap<>();
+        
+        // Initialize first window
+        for (int i = 0; i < minSize; i++) {
+            char c = s.charAt(i);
+            if (freq[c - 'a']++ == 0) distinct++;
+        }
+        
+        if (distinct <= maxLetters) {
+            countMap.put(s.substring(0, minSize), 1);
+        }
+        
+        // Slide window
+        for (int i = 1; i <= n - minSize; i++) {
+            // Remove left character
+            char left = s.charAt(i - 1);
+            if (--freq[left - 'a'] == 0) distinct--;
             
-        }
-        int mx =0;
-        for(Map.Entry<String, Integer> v : mp.entrySet()){
-            if(mx<v.getValue()){
-                mx=v.getValue();
+            // Add right character
+            char right = s.charAt(i + minSize - 1);
+            if (freq[right - 'a']++ == 0) distinct++;
+            
+            // Update count if valid
+            if (distinct <= maxLetters) {
+                String substr = s.substring(i, i + minSize);
+                countMap.put(substr, countMap.getOrDefault(substr, 0) + 1);
             }
         }
-        return mx;
-    }
-
-    public HashSet<Character> reSet(String cur){
-        HashSet<Character> st = new HashSet<>();
-        for(int i=0; i<cur.length(); i++){
-            st.add(cur.charAt(i));
-        }
-        return st;
+        
+        return countMap.values().stream().max(Integer::compare).orElse(0);
     }
 }
