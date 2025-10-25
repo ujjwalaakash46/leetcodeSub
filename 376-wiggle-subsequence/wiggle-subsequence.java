150b7d240505e855c7c0e438030516557a5fdaeb
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2025-10-25 20:59:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (21.45%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.java
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -1,25 +1,14 @@
 class Solution {
-    public int wiggleMaxLength(int[] n) {
-        
-        int c=1, p=n[0];
-        if(n.length==1) return c;
-        boolean di=true, diSet=false;
-        for(int i=1; i<n.length; i++){
-            if(!diSet && p!=n[i] ){
-                di=p<n[i];
-                p=n[i];
-                c++;
-                diSet=true;
-                continue;
-            }
-            if((di && p>n[i]) || (!di && p<n[i])){
-                di=!di;
-                p=n[i];
-                c++;
-            }else{
-                if((di && p<n[i])||(!di && p>n[i])) p=n[i];
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        int up = 1, down = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i-1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i-1]) {
+                down = up + 1;
             }
         }
-        return c;
+        return Math.max(up, down);
     }
 }
