a47bf7ca9dac4af1617f6c29d562b909bea5dbff
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2025-11-14 01:10:13 +0530

    Time: 1 ms (85.04%) | Memory: 43.9 MB (7.69%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -16,48 +16,18 @@
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
         
-        Deque<TreeNode> q1 = new ArrayDeque<>();
-        Deque<TreeNode> q2 = new ArrayDeque<>();
-
-        q2.offer(root);
-        boolean nf=false;
-        boolean lr=false;
-
-        //if null in same, next node should be null and next row zero.
-        //if null, next-q2 should be zero. 
-        while(!q2.isEmpty() || !q1.isEmpty()){
-            // System.out.println("l1");
-            while(!q1.isEmpty()){
-                // System.out.println("l2");
-                TreeNode r = q1.poll();
-                // if(r!=null)System.out.println(r.val);
-                if(r.left==null){
-                    nf=true;
-                }else{
-                    if(nf==true) return false;
-                    q2.offer(r.left);
-                }
-
-                if(r.right==null){
-                    nf=true;
-                }else{
-                    if(nf==true) return false;
-                    q2.offer(r.right);
-                }
+        Deque<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean miss = false;
+        while (q.size() > 0) {
+            TreeNode curr = q.poll();
+            if (curr == null) miss = true;
+            else {
+                if (miss) return false;
+                q.add(curr.left);
+                q.add(curr.right);
             }
-
-            if(lr && !q2.isEmpty()){
-                return false;
-            }
-            if(nf){
-                lr=true;
-            }
-
-
-            q1 = new ArrayDeque<>(q2);
-            q2.clear();
         }
-
         return true;
 
     }
