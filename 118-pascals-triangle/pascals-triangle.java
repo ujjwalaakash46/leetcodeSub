e61222ede1bbd9c90c01a4334732dd34726571f5
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2026-01-09 21:29:18 +0530

    Time: 1 ms (94.57%) | Memory: 43.6 MB (17.29%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        res.add(List.of(1));
+        for(int i=1; i<n; i++){
+            List<Integer> lr = res.get(res.size()-1);
+            List<Integer> nr = new LinkedList<>();
+            nr.add(1);
+            for(int j=1; j<lr.size(); j++){
+                int sum = lr.get(j) + lr.get(j-1);
+                nr.add(sum);
+            }
+            nr.add(1);
+
+            res.add(nr);
+        }
+
+        return res;
+    }
+}
