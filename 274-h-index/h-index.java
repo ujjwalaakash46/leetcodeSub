2ad9701f56a96756572a0e8a889f83fceb2f58f5
Ujjwal Aakash <47244018+ujjwalaakash46@users.noreply.github.com>
2026-01-11 00:39:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (83.75%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,30 +1,21 @@
 class Solution {
-    public int hIndex(int[] c) {
-        // Arrays.sort(c);
-        int cc=0, l=c.length, max=0;
-
-        // if(c[0]>l)max=l;
-
-        for(int i =0; i<=l; i++){
-            // if(c[i]>cc){
-            //     cc=c[i];
-            //     // max =Math.max(cc*, )
-            // }
-            int curMax = 0;
-            for(int j=0; j<l; j++){
-                if(c[j]>=i)curMax++;
-            }
-
-            if(curMax>=i) {
-                max = Math.max(curMax, max);
-                cc=i;
-                }
-
-            // if(i<=(l-i)){
-            //     max=c[i];
-            // }
+    public int hIndex(int[] citations) {
+    int n = citations.length;
+    int[] buckets = new int[n+1];
+    for(int c : citations) {
+        if(c >= n) {
+            buckets[n]++;
+        } else {
+            buckets[c]++;
         }
-
-        return cc;
     }
+    int count = 0;
+    for(int i = n; i >= 0; i--) {
+        count += buckets[i];
+        if(count >= i) {
+            return i;
+        }
+    }
+    return 0;
+}
 }
